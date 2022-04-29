a7aa678e71442b95602258406e9a3ebe4ab79f91
ElianeCristina <eliane_cristinanunescorreia@yahoo.com.br>
2022-04-29 17:43:30 -0300

    Update cliente-controller.js

--- src/controllers/cliente-controller.js
+++ src/controllers/cliente-controller.js
@@ -1,6 +1,14 @@
 const Cliente = require('../models/cliente-model')
 
+// chamo a exportação:
+module.exports = (app) =>{
+    const cliente = new Cliente(app)
+    app.get('/cliente', (req, res) =>{
+        res.json({"banco de dados":cliente})
+    })
+}
 // get ler
+
 
 // post inserir
 
@@ -12,4 +20,4 @@ const Cliente = require('../models/cliente-model')
 sem a necessidade de trafegar todo objeto. Para que você possa ter uma melhor entendimento, 
 vamos atualizar o projeto desenvolvido no meu artigo anterior informado acima.*/
 
-// criar uma branch no git desktop
+
