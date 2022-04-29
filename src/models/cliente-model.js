dc9cbaa9e3ae25edd8e520c0e39d96d0def5893e
ElianeCristina <eliane_cristinanunescorreia@yahoo.com.br>
2022-04-29 17:43:42 -0300

    Update cliente-model.js

--- src/models/cliente-model.js
+++ src/models/cliente-model.js
@@ -2,13 +2,18 @@ var id = 0;
 
 // Criando a classe cliente
 class Cliente{ 
-    constructor(nome, email, senha){
+    constructor(nome, cpf, endereco, tipoDeImovel, condicaoDoImovel, email, senha){
         this.id = id++; 
         this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.tipoDeImovel = tipoDeImovel;
+        this.condicaoDoImovel = condicaoDoImovel;
         this.email = email;
         this.senha = this.validarSenha(senha);
     }
-    
+    // Condições para validar cadastro
+
     validarSenha(senha) {
     if(senha.length <= 5){
         return senha;
